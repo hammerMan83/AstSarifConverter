0ae933b7b54269ee32592d5d53cf6540b534a47b
Ilan Shtokhamer <ilan.shtokhamer@checkmarx.com>
2020-11-06 17:53:10 +0200

    completed structs of sast result

--- sastResult.go
+++ sastResult.go
@@ -1,30 +1,61 @@
 package main
-package sastQuery
 
 import (
 	"encoding/json"
 )
 
-type sastResult struct {
+type SastResult struct {
 	Version string
 	ScanId string
 	CreateTime string
-	Queries []sastQuery //`json:"first_name"`
+	Queries []SastQuery //`json:"first_name"`
 }
 
-func newSastResult(jsonSastResult string) sastResult {
-	var unmarshaledSastResult sastResult
+func newSastResult(jsonSastResult string) SastResult {
+	var unmarshaledSastResult SastResult
 	json.Unmarshal([]byte(jsonSastResult), &unmarshaledSastResult)
 	return unmarshaledSastResult
 }
 
-func (r sastResult) toJSONByteArray() ([]byte) { //, error){
+func (r SastResult) toJSONByteArray() ([]byte) { //, error){
 	jsonResult, _ := json.Marshal(r)
 	return jsonResult
 }
 
-func (r sastResult) toJSON() (string) { //, error){
+func (r SastResult) toJSON() (string) { //, error){
 	jsonResult, _ := json.Marshal(r)
 	return string(jsonResult)
 }
 
+type SastQuery struct {
+	Metadata SastMetadata
+	Results []SastQueryResult
+}
+
+type SastMetadata struct {
+	Id uint64
+	QueryName string
+	GroupName string
+	Severity string
+	CweId int64
+}
+
+type SastQueryResult struct {
+	PathId uint32
+	SimilarityId int64
+	UniqueId int64
+	Nodes []SastResultNode
+}
+
+type SastResultNode struct {
+	Column uint32
+	FileName string
+	FullName string
+	Length uint32
+	Line uint64
+	MethodLine int64
+	Name string
+	NodeId uint64
+	DomType string
+	//Nodes
+}
