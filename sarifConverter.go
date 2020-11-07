f9dadb2e360b676bd3b2a39a68e090a1697ff767
Ilan Shtokhamer <ilan.shtokhamer@checkmarx.com>
2020-11-07 23:30:47 +0200

    Added CLang structs

---
+++ sarifConverter.go
@@ -0,0 +1,21 @@
+package main
+
+// import (
+// 	"encoding/json"
+// )
+
+
+//https://stackoverflow.com/questions/32708717/go-when-will-json-unmarshal-to-struct-return-error
+func convertCLangSarifToSastResult(c CLangSarif) SastResult {
+
+}
+
+// func (r SastResult) toJSONByteArray() ([]byte) { //, error){
+// 	jsonResult, _ := json.Marshal(r)
+// 	return jsonResult
+// }
+
+// func (r SastResult) toJSON() (string) { //, error){
+// 	jsonResult, _ := json.Marshal(r)
+// 	return string(jsonResult)
+// }
